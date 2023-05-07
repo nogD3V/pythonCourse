eff27cd26b95049767118f8bebc7c7fef342cefd
Lucas Silva <nogd3v@gmail.com>
2023-05-06 23:36:02 -0300

    Aulas 5 e 6

---
+++ aula5.py
@@ -0,0 +1,2 @@
+#Foi explicado o tipo de booleano, mas eu estava testando outras coisas
+a = int(input("O que é o que é clara e salgada? "))
---
+++ aula6.py
@@ -0,0 +1,12 @@
+"""
+
+Polimorfismo - Quando um objeto faz o papel de duas coisas dentro da linguagem.
+
+Concatenar - Junção de duas str
+
+"""
+
+print(int('1'), type(int('1')))
+print(type(float('1') + 1))
+print(bool(' '))
+print(str(11) + 'b')
