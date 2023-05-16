0dfe2c8c23bb9bf15417ceddf202de6ddaaba4db
Lucas Silva <nogd3v@gmail.com>
2023-05-16 08:12:19 -0300

    Aulas 7 a 10

---
+++ aula7.py
@@ -0,0 +1,5 @@
+#Variaveis são usadas para salvar algo na memória do computador.
+#PEP8: Inicie variáveis com letras minusculas, pode usar números e underline.
+
+nome_completo = 'Lucas Nogueira'
+print(nome_completo)
